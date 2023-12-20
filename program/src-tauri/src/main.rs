bb7b35726e0df61d00d1afc36c2fe3bb4cf16f44
Matheus Okamoto <128179754+tomokao@users.noreply.github.com>
2023-12-20 19:58:53 -0300

    Implement basic settings reading

--- program/src-tauri/src/main.rs
+++ program/src-tauri/src/main.rs
@@ -1,10 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use program::{commands::*, windows::spawn_first_time_setup_window};
+use std::{path::Path, sync::RwLock};
+
+use program::{
+    commands::*,
+    settings::{SettingsFile, DEFAULT_CONFIG_FILE_PATH},
+    state::AppState,
+    windows::spawn_first_time_setup_window,
+};
+use tauri::Manager;
 
 fn main() {
+    let app_state = AppState::default();
+
+    if Path::new(DEFAULT_CONFIG_FILE_PATH.as_str()).exists() {
+        let settings_file = SettingsFile::read_from_file(DEFAULT_CONFIG_FILE_PATH.as_str())
+            .expect("could not read default config file");
+        app_state.settings.get_or_init(|| {
+            RwLock::new(
+                settings_file
+                    .into_settings()
+                    .expect("could not read config file"),
+            )
+        });
+    }
+
     let app = tauri::Builder::default()
+        .manage(app_state)
         .invoke_handler(tauri::generate_handler![
             set_config_dir,
             program::windows::window_loaded,
@@ -13,7 +36,13 @@ fn main() {
         .build(tauri::generate_context!())
         .expect("error while building tauri application");
 
-    spawn_first_time_setup_window(&app).expect("could not spawn first time setup window");
+    {
+        let state = app.state::<AppState>();
+
+        if state.settings.get().is_none() {
+            spawn_first_time_setup_window(&app).expect("could not spawn first time setup window");
+        }
+    }
 
     app.run(|_, _| {});
 }
--- program/src-tauri/src/settings.rs
+++ program/src-tauri/src/settings.rs
@@ -1,15 +1,75 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use api::database::{dictionary::cdb::CDBDictionary, Database};
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+pub static DEFAULT_CONFIG_DIRECTORY_PATH: Lazy<String> = Lazy::new(|| {
+    dirs::config_dir()
+        .expect("default config directory path doesn't exist (unsupported OS?)")
+        .join("yomisama")
+        .to_str()
+        .expect("default config directory path should be valid UTF-8")
+        .to_owned()
+});
+
+pub static DEFAULT_CONFIG_FILE_PATH: Lazy<String> = Lazy::new(|| {
+    dirs::config_dir()
+        .expect("default config directory path doesn't exist (unsupported OS?)")
+        .join("yomisama/config.json")
+        .to_str()
+        .expect("default config directory path should be valid UTF-8")
+        .to_owned()
+});
 
 #[derive(Serialize, Deserialize)]
 pub struct Settings {
     pub database: Database<CDBDictionary>,
+    #[serde(skip)]
+    path: PathBuf,
+}
+
+impl Settings {
+    fn read_from_file<P: AsRef<Path>>(path: P) -> Result<Self, SettingsFileReadError> {
+        let mut result = serde_json::from_slice::<Self>(&std::fs::read(&path)?)
+            .map_err(SettingsFileReadError::Deserialize);
+
+        if let Ok(ref mut settings) = result {
+            settings.path = path.as_ref().to_path_buf();
+        }
+
+        result
+    }
+
+    pub fn get_path(&self) -> &Path {
+        &self.path
+    }
 }
 
 #[derive(Serialize, Deserialize)]
 pub enum SettingsFile {
     Settings(Settings),
     Redirect(PathBuf),
+}
+
+impl SettingsFile {
+    pub fn read_from_file<P: AsRef<Path>>(path: P) -> Result<Self, SettingsFileReadError> {
+        serde_json::from_slice(&std::fs::read(path)?).map_err(SettingsFileReadError::Deserialize)
+    }
+
+    pub fn into_settings(self) -> Result<Settings, SettingsFileReadError> {
+        match self {
+            Self::Settings(s) => Ok(s),
+            Self::Redirect(path) => Settings::read_from_file(path),
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum SettingsFileReadError {
+    #[error("settings file IO error")]
+    Io(#[from] std::io::Error),
+    #[error("settings file deserialization error")]
+    Deserialize(#[from] serde_json::Error),
 }
