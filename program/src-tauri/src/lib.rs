536df7e6e80cbaf191dd2ec1ac1590d34931e772
Matheus Okamoto <128179754+tomokao@users.noreply.github.com>
2023-12-23 23:09:44 -0300

    Better payload loading and basic querying stuff

--- program/src-tauri/src/lib.rs
+++ program/src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod commands;
 pub mod config;
+pub mod query;
 pub mod state;
 pub mod tray;
 pub mod windows;
--- program/src/App.svelte
+++ program/src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri";
   import { appWindow } from "@tauri-apps/api/window";
+  import type { ComponentType, SvelteComponent } from "svelte";
   import type { Payload } from "./bindings";
   import FirstTimeSetup from "./lib/FirstTimeSetup.svelte";
   import Loading from "./lib/Loading.svelte";
+  import Query from "./lib/Query.svelte";
+
+  const components = { FirstTimeSetup, Query };
 
   let payload: Payload | undefined = undefined;
 
@@ -18,12 +22,16 @@
 
     payload = await invoke("window_loaded");
   })();
+
+  const getComponent = (): ComponentType<SvelteComponent> =>
+    components[payload!.component];
+  const getProps = () => (({ component, ...props }) => props)(payload!);
 </script>
 
 {#await init}
   <Loading />
 {:then _}
   <main class="container">
-    <FirstTimeSetup {payload} />
+    <svelte:component this={getComponent()} {...getProps()} />
   </main>
 {/await}
